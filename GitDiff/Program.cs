2c3c35620c7acbed807f15eac3d28f8beb410470
Matias Blanc <blancmaty@hotmail.com>
2022-09-24 13:25:55 -0400

    Add project files.

---
+++ GitDiff/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+
+namespace GitDiff
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir = @"C:\Projects\Visual Studio\WaveFunctionCollapse";
+
+            Collection<PSObject> results;
+
+            using (PowerShell ps = PowerShell.Create())
+            {
+                ps.AddScript("cd " + "\"" + dir + "\"");
+                ps.AddScript("git diff HEAD^..HEAD~3");
+
+                results = ps.Invoke();
+
+                if ((results is not null) && (results.Count > 0))
+                {
+                    Console.WriteLine(String.Join(Environment.NewLine, results));
+                }
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
