969edb22e741ec272a5600a863e1e6b7faea0e10
Matias Blanc <blancmaty@hotmail.com>
2022-11-20 12:08:50 -0500

    Moved consumer class

---
+++ GitDiff/Consumer.cs
@@ -0,0 +1,71 @@
+﻿using GitDiff.Smells;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDiff
+{
+    public class Consumer
+    {
+        public Consumer(string resultDirectory)
+        {
+            ResultDirectory = resultDirectory;
+
+            Task.Factory.StartNew(() => ThreadMain(), TaskCreationOptions.LongRunning);
+        }
+
+        public string ResultDirectory
+        { get; }
+
+        public ConcurrentQueue<CodeSmellResults> ResultsQueue
+        { get; } = new ConcurrentQueue<CodeSmellResults>();
+
+        public bool ResultsQueueIsEmpty
+        { get { return ResultsQueue.IsEmpty; } }
+
+        public delegate void ConsumerCallback();
+
+        public ConsumerCallback OnStart
+        { get; set; } = null;
+
+        public ConsumerCallback OnEnd
+        { get; set; } = null;
+
+        private void ThreadMain()
+        {
+            while (ResultsQueueIsEmpty) Thread.Sleep(TimeSpan.FromMilliseconds(50));
+            if (OnStart is not null) OnStart();
+
+            while (true)
+            {
+                while (ResultsQueue.TryDequeue(out CodeSmellResults codeSmellResults))
+                {
+                    ConsolePrint(codeSmellResults);
+                    CSVPrint(codeSmellResults);
+                }
+
+                Thread.Sleep(TimeSpan.FromMilliseconds(10));
+            }
+
+            if (OnEnd is not null) OnEnd();
+        }
+
+        public void ConsolePrint(CodeSmellResults codeSmellResults)
+        {
+            string toPrint = codeSmellResults.Print();
+
+            if (!string.IsNullOrWhiteSpace(toPrint))
+            {
+                Console.WriteLine(toPrint);
+            }
+        }
+
+        public void CSVPrint(CodeSmellResults codeSmellResults)
+        {
+            codeSmellResults.SaveToCSV(ResultDirectory);
+        }
+    }
+}
